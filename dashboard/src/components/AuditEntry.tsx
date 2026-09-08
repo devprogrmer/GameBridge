a516ccb8c76101086fad20ea7506327d4eb05bbe
devprogrmer <amyrmhsnmlky3@gmail.com>
2026-09-08 16:51:55 +0330

    Add Rebecca Phase14 audit security foundation

---
+++ dashboard/src/components/AuditEntry.tsx
@@ -0,0 +1,3 @@
+export default function AuditEntry({action}:{action:string}){
+  return <div>{action}</div>;
+}
---
+++ dashboard/src/components/SecurityEvent.tsx
@@ -0,0 +1,3 @@
+export default function SecurityEvent({event}:{event:string}){
+  return <div>{event}</div>;
+}
